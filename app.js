51fb2f9757e4bcfe4a7a1dbae3e8f11fcd09631b
Bskakel <bskakel.ptech@gmail.com>
2015-12-19 12:26:49 -0400

    Navbar fix

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var main = function() {
+
 $('.dropdown-menu').hide();
+$('.dropdown-menu2').hide();
+$('.dropdown-menu3').hide();
 
 $('.dropdown-toggle').click(function() {
     $('.dropdown-menu').toggle();
+});
+
+$('.dropdown-toggle2').click(function() {
+    $('.dropdown-menu2').toggle();
+});
+
+$('.dropdown-toggle3').click(function() {
+    $('.dropdown-menu3').toggle();
 });
 
 }
